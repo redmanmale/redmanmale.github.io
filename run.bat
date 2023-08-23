37b028fe4f9d9f284952e22ccbf23b22f849a351
redmanmale <redmanmale@gmail.com>
2023-08-23 20:45:52 +0300

    remove update scripts

--- run.bat
+++ run.bat
@@ -1,5 +1,4 @@
 del tag /q
 del _site /q
 python tag_generator.py
-python update.py
 jekyll serve --host=0.0.0.0
--- update.bat
+++
@@ -1,4 +0,0 @@
-python update.py
-git add index.html personal.html
-git commit -m "Update personal posts" -S
-git push origin HEAD
