104bdf854340b391e72f03dd7ca8adb070e996c9
redmanmale <redmanmale@gmail.com>
2023-08-24 22:47:26 +0300

    support other image types than jpg; show percentage during processing

--- process_images.py
+++ process_images.py
@@ -5,8 +5,13 @@ post = sys.argv[1]
 folder_target = os.path.join(root, "images", post)
 folder_originals = os.path.join(folder_target, "originals")
 
-for filename in os.listdir(folder_originals):
-    print("Processing " + filename)
+files = os.listdir(folder_originals)
+files_count = len(files)
+counter = 0
+for filename in files:
+    os.system('cls')
+    percent = counter * 100 / files_count
+    print("done: " + str(counter) + "/" + str(files_count) + ", " + str(round(percent)) + "%")
     pos = filename.find("-")
     if pos == -1:
         inp = os.path.join(folder_originals, filename)
@@ -17,8 +22,17 @@ for filename in os.listdir(folder_originals):
         name = filename[0:pos]
         part = int(filename[pos + 1:filename.rfind(".")])
         if part == 1:
-            inp1 = os.path.join(folder_originals, name + "-1.jpg")
-            inp2 = os.path.join(folder_originals, name + "-2.jpg")
+            tmp1, ext1 = os.path.splitext(filename)
+            inp1 = os.path.join(folder_originals, name + "-1" + ext1)
+
+            tmp2, ext2 = os.path.splitext(files[counter + 1])
+            inp2 = os.path.join(folder_originals, name + "-2" + ext2)
+
             outp = os.path.join(folder_target, name + ".jpg")
             cmd = "c2post \"" + inp1 + "\" \"" + inp2 + "\" \"" + outp + "\""
             os.system(cmd)
+    counter += 1
+
+os.system('cls')
+percent = counter * 100 / files_count
+print("done: " + str(counter) + "/" + str(files_count) + ", " + str(round(percent)) + "%")
